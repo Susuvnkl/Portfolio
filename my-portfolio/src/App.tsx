8012c3ceb3dc04c2b8d91cc5905e1689dca11c9c
David Bernstein <susuvnkl6@gmail.com>
2023-12-14 10:08:05 +0200

    changed routing method

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,12 +1,21 @@
-import "./App.scss";
-import Page from "./components/Page/Page";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import ContactPage from "./pages/ContactPage/ContactPage";
+import Layout from "./components/layout/Layout";
+import useActivePage from "./hooks/useActivePage";
 
 function App() {
+  useActivePage();
   return (
     <>
-      <div>
-        <Page />
-      </div>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Route>
+      </Routes>
     </>
   );
 }
--- my-portfolio/src/main.tsx
+++ my-portfolio/src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
-import { RouterProvider } from "react-router-dom";
-import router from "./Router.tsx";
-import BackgroundLayout from "./components/BackgroundLayout/BackgroundLayout.tsx";
+// import "./index.scss";
+import "./styles/global.scss";
+import App from "./App.tsx";
+import { BrowserRouter as Router } from "react-router-dom"; // import BackgroundLayout from "./components/BackgroundLayout/BackgroundLayout.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BackgroundLayout>
-      <RouterProvider router={router} />
-    </BackgroundLayout>
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
