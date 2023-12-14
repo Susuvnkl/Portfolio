a5af5e2485a5fe16e1af5634fcaaeab3ecee5467
David Bernstein <susuvnkl6@gmail.com>
2023-12-14 10:10:35 +0200

    added window size hook

---
+++ my-portfolio/src/hooks/useWindowSize.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState<{
+    width: number | undefined;
+    height: number | undefined;
+  }>({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    console.log("test", {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize); // Clean up
+  }, [window.innerWidth, window.innerHeight]);
+
+  return windowSize;
+}
+
+export default useWindowSize;
