d9ef184488e997b617bb21d03c17fbcfe3bf4c65
David Bernstein <susuvnkl6@gmail.com>
2023-12-17 19:03:21 +0200

    refactoring and routing change

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,19 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import ContactPage from "./pages/ContactPage/ContactPage";
 import Layout from "./components/layout/Layout";
-import useActivePage from "./hooks/useActivePage";
+import MainPage from "./pages/MainPage/MainPage";
 
 function App() {
-  useActivePage();
   return (
     <>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
+          <Route index element={<MainPage />} />
         </Route>
       </Routes>
     </>
