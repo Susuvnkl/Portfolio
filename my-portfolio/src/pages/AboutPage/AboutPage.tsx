3bfa800821cd2b1a87bb1a3509db2f4d8d0b9b57
David Bernstein <susuvnkl6@gmail.com>
2023-12-20 12:32:35 +0200

    moved content to content page

--- my-portfolio/src/pages/AboutPage/AboutPage.tsx
+++ my-portfolio/src/pages/AboutPage/AboutPage.tsx
@@ -1,9 +1,43 @@
 import styles from "./AboutPage.module.scss";
 
 function AboutPage() {
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <div id="about" className={styles.PageSection}>
-      About Content
+      <div className={styles.AboutPage}>
+        <div className={styles.content}>
+          <div>
+            <p>
+              Passionate and self-driven full-stack developer ready to bring innovation to life.
+            </p>
+            <p>
+              With a tireless work ethic and a natural curiosity for all things tech, I thrive on
+              challenges and am dedicated to creating seamless user experiences.
+            </p>
+            <p>
+              {/* Whether it's coding on the front end to captivate users or optimizing */}
+              {/* server performance on the back end, I embrace each aspect of web development.  */}
+              My journey is fueled by a love for learning, a meticulous attention to detail, and an
+              eagerness to contribute to cutting-edge projects.
+            </p>
+            <p>
+              Join me as I navigate the ever-evolving world of technology, constantly evolving, and
+              turning ideas into reality. Let's build something extraordinary together!
+            </p>
+            <p>
+              <button className={styles.contactMeButton} onClick={() => scrollToSection("contact")}>
+                Contact me
+              </button>{" "}
+              and lets build something extraordinary together!{" "}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- my-portfolio/src/pages/HomePage/HomePage.tsx
+++ my-portfolio/src/pages/HomePage/HomePage.tsx
@@ -15,31 +15,6 @@ function HomePage() {
           <div>
             <h1>Welcome to my digital space! I'm David Web Developer</h1>
           </div>
-          <div>
-            <p>
-              Passionate and self-driven full-stack developer ready to bring innovation to life.
-            </p>
-            <p>
-              With a tireless work ethic and a natural curiosity for all things tech, I thrive on
-              challenges and am dedicated to creating seamless user experiences.
-            </p>
-            <p>
-              {/* Whether it's coding on the front end to captivate users or optimizing */}
-              {/* server performance on the back end, I embrace each aspect of web development.  */}
-              My journey is fueled by a love for learning, a meticulous attention to detail, and an
-              eagerness to contribute to cutting-edge projects.
-            </p>
-            <p>
-              Join me as I navigate the ever-evolving world of technology, constantly evolving, and
-              turning ideas into reality. Let's build something extraordinary together!
-            </p>
-            <p>
-              <button className={styles.contactMeButton} onClick={() => scrollToSection("contact")}>
-                Contact me
-              </button>{" "}
-              and lets build something extraordinary together!{" "}
-            </p>
-          </div>
         </div>
         <div className={styles.BinaryMouseTrackContainer}>
           <BinaryMatrix textSize={14} />
