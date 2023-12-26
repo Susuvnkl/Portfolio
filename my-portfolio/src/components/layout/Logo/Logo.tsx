2e01d0240f937c2c8b79f5c7d55caa6c977805f9
David Bernstein <susuvnkl6@gmail.com>
2023-12-26 11:11:01 +0200

    Logo modified

---
+++ my-portfolio/src/components/layout/Logo/Logo.tsx
@@ -0,0 +1,10 @@
+import styles from "./Logo.module.scss";
+
+function Logo() {
+  return (
+    <div className={styles.Logo}>
+      <h2>{"<DavidBernstein /> "}</h2>
+    </div>
+  );
+}
+export default Logo;
