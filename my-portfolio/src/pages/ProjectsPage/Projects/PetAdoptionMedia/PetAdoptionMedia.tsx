ff80bf4a21290b90847ba0adcc4348429aa93f1c
David Bernstein <susuvnkl6@gmail.com>
2024-01-30 14:54:20 +0200

    Unused file errors fix

--- my-portfolio/src/pages/ProjectsPage/Projects/PetAdoptionMedia/PetAdoptionMedia.tsx
+++ my-portfolio/src/pages/ProjectsPage/Projects/PetAdoptionMedia/PetAdoptionMedia.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import PetAdoptionMobile1 from "../../../../components/common/Icons/PetAdoptionMobile1/PetAdoptionMobile1";
 import PetAdoptionMobile2 from "../../../../components/common/Icons/PetAdoptionMobile2/PetAdoptionMobile2";
 import PetAdoptionWeb from "../../../../components/common/Icons/PetAdoptionWeb/PetAdoptionWeb";
@@ -7,37 +6,13 @@ import styles from "./PetAdoptionMedia.module.scss";
 const PET_ADOPTION_URL: string = "https://petadoption-client.vercel.app/";
 
 function PetAdoptionMedia() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 768px)");
-    if (mediaQuery.matches) {
-      setIsMobile(true);
-    } else {
-      setIsMobile(false);
-    }
-
-    const handleResize = () => {
-      if (mediaQuery.matches) {
-        setIsMobile(true);
-      } else {
-        setIsMobile(false);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
   const openPetAdoptionUrl = () => {
     window.open(PET_ADOPTION_URL, "_blank");
   };
   return (
     <div className={styles.PetAdoptionMedia}>
       <div className={styles.PetWeb} onClick={openPetAdoptionUrl}>
-        <PetAdoptionWeb mode={isMobile ? "mobile" : undefined} />
+        <PetAdoptionWeb />
       </div>
       <div className={styles.PhoneWrapper}>
         <div className={styles.PetMobile1} onClick={openPetAdoptionUrl}>
